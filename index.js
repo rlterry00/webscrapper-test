44d66e3e6bb997adfda399d23ab4daf5df6d833c
rlterry00 <chuckterry00@gmail.com>
2021-09-22 15:53:17 -0400

    able to find black therapist in charlotte by name. will work more on getting other elements

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const cheerio = require("cheerio");
 const pretty = require("pretty");
 
 const url =
-  "https://www.psychologytoday.com/us/therapists/african-american/nc/charlotte";
+    "https://www.psychologytoday.com/us/therapists/african-american/nc/charlotte";
+
+  
+const scrapeData = async () => {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    console.log("Black Therapist in Charlotte:", pretty($(".result-name").text()));
+} 
+scrapeData();
