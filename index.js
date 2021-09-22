b5520e850f61cd6b67457ef9ff3e7215d8571155
rlterry00 <chuckterry00@gmail.com>
2021-09-22 14:37:14 -0400

    setting up dependencies

---
+++ index.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const pretty = require("pretty");
+
+const url =
+  "https://www.psychologytoday.com/us/therapists/african-american/nc/charlotte";
